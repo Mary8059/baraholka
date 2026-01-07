271d26534df7169c8ccfcc8f5e0cc24c475f3dcb
Mary8059 <marivane8059@gmail.com>
2026-01-07 21:55:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+'use strict';
+
+ 
+document.querySelectorAll('.menu-link').forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add('active');
+  }
+});
+
+
+
+function gir() {
+    let nums = document.getElementById('gir').className; 
+    if(nums == 'gir_1') {document.getElementById('gir').className='gir_2';}  
+    if(nums == 'gir_2') {document.getElementById('gir').className='gir_3';}  
+    if(nums == 'gir_3') {document.getElementById('gir').className='gir_1';} 
+   }  
+    setInterval(gir, 500); 
+
+
+ document.addEventListener('DOMContentLoaded', () => {
+  const polaroids = document.querySelectorAll('.polaroid');
+
+  polaroids.forEach(card => {
+    const angle = Math.floor(Math.random() * 8 - 4); // от -4 до +3
+    card.style.setProperty('--random-rotation', `${angle}deg`);
+  });
+});
+
+  
+/*
+snowFlurry JS - version 2.0
+Copyright Ð’Â© 2015 S.W. Clough (https://www.html5andbeyond.com)
+Licensed Under MIT
+*/
+(function ($) {
+  $.fn.snowFlurry = function (options) {
+    var s = $.extend({
+      maxSize: 5,
+      numberOfFlakes: 25,
+      minSpeed: 10,
+      maxSpeed: 15,
+      color: '#fff',
+      timeout: 0
+    }, options);
+    var windowWidth = $(window).innerWidth(),
+      WidthArray = [],
+      DelayArray = [],
+      animateArray = [],
+      flakeSize = [],
+      snowInterval;
+    if (s.maxSize <= 10) {
+      for (var i = 1; i < s.maxSize; i++) {
+        flakeSize.push(i);
+      }
+    } else {
+      for (var i = 1; i < 10; i++) {
+        flakeSize.push(i);
+      }
+    }
+    for (var i = 0; i < windowWidth - 20; i++) {
+      WidthArray.push(i);
+    }
+    for (var i = 0; i < s.numberOfFlakes; i++) {
+      $('<div class="sf-snow-flake"></div>').appendTo('body');
+    }
+    for (var i = 0; i < 10; i++) {
+      DelayArray.push(i);
+    }
+    for (var i = s.minSpeed; i < s.maxSpeed; i++) {
+      animateArray.push(i);
+    }
+    function getRandomFlakeSize() {
+      var item = flakeSize[Math.floor(Math.random() * flakeSize.length)];
+      return item;
+    }
+    function getRandomPosition() {
+      var item = WidthArray[Math.floor(Math.random() * WidthArray.length)];
+      return item;
+    }
+    function getRandomDelay() {
+      var item = DelayArray[Math.floor(Math.random() * DelayArray.length)];
+      return item * 1000;
+    }
+    function getRandomAnimation() {
+      var item = animateArray[Math.floor(Math.random() * animateArray.length)];
+      return item * 1000;
+    }
+    $('.sf-snow-flake').each(function () {
+      var elem = $(this);
+      elem.attr('data-speed', getRandomAnimation());
+      elem.attr('data-delay', getRandomDelay());
+      var elemSpeed = elem.attr('data-speed'),
+        elemDelay = elem.attr('data-delay');
+      var flakeSize = getRandomFlakeSize();
+      elem.css({
+        'width': flakeSize,
+        'height': flakeSize,
+        'border-radius': flakeSize / 2,
+        'background-color': s.color,
+        'box-shadow': '0 0 2px 1px' + s.color
+      })
+      function activateAnim() {
+        setTimeout(function () {
+          elem.css('left', getRandomPosition());
+          elem.addClass('sf-snow-anim');
+          elem.css('transition', 'top ' + elemSpeed / 1000 + 's linear');
+          setTimeout(function () {
+            elem.css('transition', '');
+            elem.removeClass('sf-snow-anim');
+          }, elemSpeed);
+        }, elemDelay);
+      }
+      if (device.mobile() || device.tablet() || Modernizr.touch || $('html').hasClass('no-csstransitions')) { } else if (device.desktop()) {
+        activateAnim();
+        snowInterval = setInterval(function () {
+          activateAnim();
+        }, +elemDelay + +elemSpeed);
+      }
+      if (s.timeout != 0) {
+        setTimeout(function () {
+          clearInterval(snowInterval);
+          $('.sf-snow-flake').fadeOut(1500, function () {
+            $(this).remove();
+          })
+        }, s.timeout * 1000);
+      }
+    });
+  };
+}(jQuery));
+jQuery(document).ready(function ($) {
+  $(document).snowFlurry({
+    maxSize: 10,
+    numberOfFlakes: 100,
+    minSpeed: 10,
+    maxSpeed: 20,
+    color: '#fff',
+    timeout: 0
+  });
+});
+
+ 
+
+
